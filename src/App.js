0d5ffc7511c58b34e614d1565bb0f3c4c05013f4
DZ <adrianxwijaya@gmail.com>
2022-08-26 18:52:08 +0700

    google sign in setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { auth, signInWithGoogle } from './Firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function App() {
+	const [user] = useAuthState(auth);
+
 	return (
 		<div className='App'>
-			<header> </header>
+			<section>{user ? <SignOut /> : <SignIn />}</section>
 		</div>
+	);
+}
+
+function SignIn() {
+	return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+	return (
+		auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
 	);
 }
 
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from '@firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,5 +20,18 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+	signInWithPopup(auth, provider)
+		.then((result) => {
+			console.log(result);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
 
 export const db = getFirestore(app);
