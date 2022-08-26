eb3937a07f3ef7f15305edcdf6ef642df0406dc5
DZ <adrianxwijaya@gmail.com>
2022-08-27 01:36:46 +0700

    use leaflet map

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './Home.scss';
+
+import { Icon } from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 import SignOutBtn from '../../Components/SignOutBtn/SignOutBtn';
 
 export default function Home() {
 	return (
-		<div>
-			<SignOutBtn />
-		</div>
+		<>
+			<MapContainer
+				center={[-6.17511, 106.865036]}
+				zoom={13}
+				scrollWheelZoom={false}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				/>
+			</MapContainer>
+			<div>
+				<SignOutBtn />
+			</div>
+		</>
 	);
 }
