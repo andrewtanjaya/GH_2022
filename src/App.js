3b952044f43158e4d6004a16fd0a205dcbca355b
andrewtanjaya <andrewtanjaya21@gmail.com>
2022-08-26 20:28:47 +0700

    PWA feature enabled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import { PAGE_MODE_OFFLINE, PAGE_MODE_ONLINE } from './Constants';
 
 function App() {
+	const [mode, setMode] = useState(PAGE_MODE_ONLINE)
+
+	// !! cache fetched user here !!
+	// get user [logged in user] from db, if success set data to LocalStorage + set to Online Mode
+	// if failed, get data from localstorage and set data to state + set to Offline Mode
+	
 	return (
 		<div className='App'>
-			<header> </header>
+			{
+				mode === PAGE_MODE_OFFLINE ? <p>Your are offline</p> :
+				<p>Your are online</p>
+			}
 		</div>
 	);
 }
