64ff9fe0120e415307073c3c9bc0b72ec7af918b
DZ <adrianxwijaya@gmail.com>
2022-08-27 13:00:33 +0700

    add custom icon on marker

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -21,179 +21,184 @@ import EventMarker from '../../Components/EventMarker/EventMarker';
 import { updatePosition } from '../../Database';
 
 function LocationMarker({ eventMarker, user }) {
-	const [position, setPosition] = useState(null);
-	const map = useMapEvents({
-		click() {
-			map.locate();
-		},
-		locationfound(e) {
-			setPosition(e.latlng);
-			map.flyTo(e.latlng, map.getZoom());
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
 
-			updatePosition(sessionStorage.getItem('uid'), e.latlng);
-		},
-	});
+      updatePosition(sessionStorage.getItem('uid'), e.latlng);
+    },
+  });
 
-	useEffect(() => {
-		map.locate();
-	}, []);
+  useEffect(() => {
+    map.locate();
+  }, []);
 
-	return position === null ? (
-		<></>
-	) : (
-		<AddMarker user={user} ev={eventMarker} position={position} />
-	);
+  return position === null ? (
+    <></>
+  ) : (
+    <AddMarker user={user} event={eventMarker} position={position} />
+  );
 }
 
 export default function Home() {
-	const [mode, setMode] = useState(PAGE_MODE_ONLINE);
+  const [mode, setMode] = useState(PAGE_MODE_ONLINE);
 
-	const [notification, setNotification] = useState({ title: '', body: '' });
-	const [show, setShow] = useState(false);
-	const [deviceTokens, setDeviceTokens] = useState([]);
-	const [cachedUser, setCachedUser] = useState({});
+  const [notification, setNotification] = useState({ title: '', body: '' });
+  const [show, setShow] = useState(false);
+  const [deviceTokens, setDeviceTokens] = useState([]);
+  const [cachedUser, setCachedUser] = useState({});
 
-	const [mockEvent, setMockEvent] = useState(
-		new Event(
-			sessionStorage.getItem('uid'),
-			'SOS',
-			'SOS nih butuh bantuan!',
-			0,
-			0,
-			[]
-		)
-	);
-	// TODO
-	// !! cache fetched user here !!
-	// get user [logged in user] from db, if success set data to LocalStorage + set to Online Mode
-	// if failed, get data from localstorage and set data to state + set to Offline Mode
+  const [mockEvent, setMockEvent] = useState(
+    new Event(
+      sessionStorage.getItem('uid'),
+      'SOS',
+      'SOS nih butuh bantuan!',
+      0,
+      0,
+      [],
+    ),
+  );
+  // TODO
+  // !! cache fetched user here !!
+  // get user [logged in user] from db, if success set data to LocalStorage + set to Online Mode
+  // if failed, get data from localstorage and set data to state + set to Offline Mode
 
-	const q = query(usersRef, where('uid', '==', sessionStorage.getItem('uid')));
-	const [currentUser, loading, error] = useCollectionData(q, {
-		idField: 'uid',
-	});
-	const [events, loadingEvents, errorEvents] = useCollectionData(eventRef, {
-		idField: 'uid',
-	});
-	const [allUser, loadingAllUser, errorAllUser] = useCollectionData(usersRef, {
-		idField: 'uid',
-	});
+  const q = query(usersRef, where('uid', '==', sessionStorage.getItem('uid')));
+  const [currentUser, loading, error] = useCollectionData(q, {
+    idField: 'uid',
+  });
+  const [events, loadingEvents, errorEvents] = useCollectionData(eventRef, {
+    idField: 'uid',
+  });
+  const [allUser, loadingAllUser, errorAllUser] = useCollectionData(usersRef, {
+    idField: 'uid',
+  });
 
-	useEffect(() => {
-		if (error) {
-			setMode(PAGE_MODE_OFFLINE);
-			setCachedUser(localStorage.getItem('user'));
-		}
-	}, [error]);
+  useEffect(() => {
+    if (error) {
+      setMode(PAGE_MODE_OFFLINE);
+      setCachedUser(localStorage.getItem('user'));
+    }
+  }, [error]);
 
-	useEffect(() => {
-		if (currentUser) localStorage.setItem('user', currentUser);
-	}, [currentUser]);
+  useEffect(() => {
+    if (currentUser) localStorage.setItem('user', currentUser);
+  }, [currentUser]);
 
-	onMessageListener()
-		.then((payload) => {
-			setShow(true);
-			setNotification({
-				title: payload.notification.title,
-				body: payload.notification.body,
-			});
-			console.log(payload);
-		})
-		.catch((err) => console.log('failed: ', err));
+  onMessageListener()
+    .then((payload) => {
+      setShow(true);
+      setNotification({
+        title: payload.notification.title,
+        body: payload.notification.body,
+      });
+      console.log(payload);
+    })
+    .catch((err) => console.log('failed: ', err));
 
-	const sendPush = () => {
-		console.log(process.env.REACT_APP_FIREBASE_CLOUD_MESSAGING_KEY_API);
+  const sendPush = () => {
+    console.log(process.env.REACT_APP_FIREBASE_CLOUD_MESSAGING_KEY_API);
 
-		const url = 'https://fcm.googleapis.com/fcm/send';
+    const url = 'https://fcm.googleapis.com/fcm/send';
 
-		fetch(url, {
-			method: 'POST',
-			body: JSON.stringify({
-				registration_ids: deviceTokens,
-				notification: {
-					title: 'Test Notif From App',
-					body: 'Test Notif From App',
-				},
-			}),
-			headers: {
-				'Content-Type': 'application/json',
-				Connection: 'keep-alive',
-				'Accept-Encoding': 'gzip, deflate, br',
-				Host: '<calculated when request is sent>',
-				'Content-Length': '<calculated when request is sent>',
-				Accept: '*/*',
-				Authorization:
-					'key=' + process.env.REACT_APP_FIREBASE_CLOUD_MESSAGING_KEY_API,
-			},
-		})
-			.then((response) => response.text())
-			.then((html) => console.log(html));
-	};
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        registration_ids: deviceTokens,
+        notification: {
+          title: 'Test Notif From App',
+          body: 'Test Notif From App',
+        },
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Connection: 'keep-alive',
+        'Accept-Encoding': 'gzip, deflate, br',
+        Host: '<calculated when request is sent>',
+        'Content-Length': '<calculated when request is sent>',
+        Accept: '*/*',
+        Authorization:
+          'key=' + process.env.REACT_APP_FIREBASE_CLOUD_MESSAGING_KEY_API,
+      },
+    })
+      .then((response) => response.text())
+      .then((html) => console.log(html));
+  };
 
-	return (
-		<div>
-			{mode === PAGE_MODE_OFFLINE ? (
-				<p>Your are offline</p>
-			) : (
-				<p>Your are online</p>
-			)}
-			<div>
-				<p>Notif Title : {notification.title}</p>
-				<p>Notif Body : {notification.body}</p>
-			</div>
-			{loading ? (
-				<pre>loading please wait...</pre>
-			) : error ? (
-				<pre>{cachedUser}</pre>
-			) : (
-				<pre>{JSON.stringify(currentUser)}</pre>
-			)}
+  return (
+    <div>
+      {mode === PAGE_MODE_OFFLINE ? (
+        <p>Your are offline</p>
+      ) : (
+        <p>Your are online</p>
+      )}
+      <div>
+        <p>Notif Title : {notification.title}</p>
+        <p>Notif Body : {notification.body}</p>
+      </div>
 
-			<button onClick={sendPush}>Send Notif</button>
+      {loading ? (
+        <pre>loading please wait...</pre>
+      ) : error ? (
+        <pre>{cachedUser}</pre>
+      ) : (
+        <pre>{JSON.stringify(currentUser)}</pre>
+      )}
 
-			<SOSBtn myEvent={mockEvent} />
-			<SignOutBtn currentUser={currentUser} />
+      <button onClick={sendPush}>Send Notif</button>
 
-			{loadingEvents ? (
-				<pre>loading Event please wait...</pre>
-			) : (
-				events.map((event) => {
-					return event ? <EventMarker key={event.uid} ev={event} /> : <></>;
-				})
-			)}
+      <SOSBtn myEvent={mockEvent} />
+      <SignOutBtn currentUser={currentUser} />
 
-			<MapContainer
-				id='map'
-				center={[-6.17511, 106.865036]}
-				zoom={13}
-				scrollWheelZoom={false}
-			>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-				/>
-				<LocationMarker user={currentUser ? currentUser[0] : null} />
+      {loadingEvents ? (
+        <pre>loading Event please wait...</pre>
+      ) : (
+        events.map((event) => {
+          return event ? <EventMarker key={event.uid} ev={event} /> : <></>;
+        })
+      )}
 
-				{loadingAllUser ? (
-					<></>
-				) : (
-					allUser.map((u) => {
-						return (
-							<AddMarker
-								key={u.uid}
-								user={u}
-								position={{
-									lat: u.latitude,
-									lng: u.longitude,
-								}}
-							/>
-						);
-					})
-				)}
-			</MapContainer>
+      <MapContainer
+        id="map"
+        center={[-6.17511, 106.865036]}
+        zoom={14}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {loading ? (
+          <></>
+        ) : (
+          <LocationMarker user={currentUser ? currentUser[0] : null} />
+        )}
 
-			<SignOutBtn />
-			<button onClick={GetCurrentLocation}>Get Current Location</button>
-		</div>
-	);
+        {loadingAllUser ? (
+          <></>
+        ) : (
+          allUser.map((u) => {
+            return (
+              <AddMarker
+                key={u.uid}
+                user={u}
+                position={{
+                  lat: u.latitude,
+                  lng: u.longitude,
+                }}
+              />
+            );
+          })
+        )}
+      </MapContainer>
+
+      <SignOutBtn />
+      <button onClick={GetCurrentLocation}>Get Current Location</button>
+    </div>
+  );
 }
--- src/Utils/AddMarker.jsx
+++ src/Utils/AddMarker.jsx
@@ -4,46 +4,65 @@ import { Marker } from 'react-leaflet';
 import PopUpWithLocation from '../Components/PopUpWithLocation/PopUpWithLocation';
 import { updateAccepted } from '../Database';
 
+import { Icon } from 'leaflet';
+
+// export const icon = new Icon({
+//   iconUrl: '/skateboarding.svg',
+//   iconSize: [25, 25],
+// });
+
 export default function AddMarker({ event, position, user }) {
-	/*
+  /*
 	description: string
 	position : {
 	 	lat: -6.17651,
 	 	lng: 106.790108,
 	 }
 	*/
-	useEffect(() => {
-		console.log('user 1', user);
-	}, []);
-	const acceptEvent = (e) => {
-		e.preventDefault();
-		const currentUid = sessionStorage.getItem('uid');
-		if (currentUid !== event.uid) {
-			if (event.accepted_uids && event.accepted_uids.length > 0) {
-				if (!event.accepted_uids.includes(currentUid)) {
-					event.accepted_uids.push(currentUid);
-					updateAccepted(event.uid, currentUid);
-				} else {
-					alert('You have accepted this event');
-				}
-			} else {
-				const uids = event.accepted_uids
-					? (event.accepted_uids[0] = currentUid)
-					: [currentUid];
-				updateAccepted(event.uid, uids);
-			}
-		} else {
-			alert('Cannot accept your own event');
-		}
-	};
-	return position === null ? null : (
-		<Marker position={position}>
-			<PopUpWithLocation
-				user={user}
-				ev={event}
-				position={position}
-				acceptCallback={acceptEvent}
-			/>
-		</Marker>
-	);
+  useEffect(() => {
+    console.log('user 1', user);
+  }, []);
+
+  const acceptEvent = (e) => {
+    e.preventDefault();
+    const currentUid = sessionStorage.getItem('uid');
+    if (currentUid !== event.uid) {
+      if (event.accepted_uids && event.accepted_uids.length > 0) {
+        if (!event.accepted_uids.includes(currentUid)) {
+          event.accepted_uids.push(currentUid);
+          updateAccepted(event.uid, currentUid);
+        } else {
+          alert('You have accepted this event');
+        }
+      } else {
+        const uids = event.accepted_uids
+          ? (event.accepted_uids[0] = currentUid)
+          : [currentUid];
+        updateAccepted(event.uid, uids);
+      }
+    } else {
+      alert('Cannot accept your own event');
+    }
+  };
+
+  return position === null && user ? null : (
+    <Marker
+      position={position}
+      icon={
+        new Icon({
+          iconUrl: user
+            ? 'https://cdn.icon-icons.com/icons2/2596/PNG/512/thinking_problem_icon_154732.png'
+            : 'https://cdn.icon-icons.com/icons2/2596/PNG/512/check_one_icon_155665.png',
+          iconSize: [25, 25],
+        })
+      }
+    >
+      <PopUpWithLocation
+        user={user}
+        event={event}
+        position={position}
+        acceptCallback={acceptEvent}
+      />
+    </Marker>
+  );
 }
