0698270f94d5565bb3697adfdf58ac19d9107092
DZ <adrianxwijaya@gmail.com>
2022-08-27 16:23:15 +0700

    temp fix

--- src/Components/UserMarkerIcon/UserMarkerIcon.jsx
+++ src/Components/UserMarkerIcon/UserMarkerIcon.jsx
@@ -4,6 +4,9 @@ import { Img } from 'react-image';
 
 import { divIcon } from 'leaflet';
 import { renderToStaticMarkup } from 'react-dom/server';
+
+import { ThreeDots } from 'react-loader-spinner';
+
 import './UserMarkerIcon.scss';
 
 export default function UserMarkerIcon({ url }) {
@@ -12,8 +15,21 @@ export default function UserMarkerIcon({ url }) {
     renderToStaticMarkup(
       <div className="marker-user-icon-marker">
         <Img
-          src={[url + './', './asset/event.png']}
+          src={[url, './asset/event.png']}
           className="marker-user-icon-img"
+          // unloader={<img src="./asset/event.png"></img>}
+          // loader={
+          //   // <ThreeDots
+          //   //   height="40"
+          //   //   width="40"
+          //   //   radius="4"
+          //   //   color="white"
+          //   //   ariaLabel="three-dots-loading"
+          //   //   wrapperStyle={{}}
+          //   //   wrapperClassName=""
+          //   //   visible={true}
+          //   // />
+          // }
         />
       </div>,
     );
