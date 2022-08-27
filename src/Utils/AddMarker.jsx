1a52ff072a97574b350df4101ccdbd9249ee2345
DZ <adrianxwijaya@gmail.com>
2022-08-27 11:57:51 +0700

    fix error and rename param

--- src/Utils/AddMarker.jsx
+++ src/Utils/AddMarker.jsx
@@ -1,49 +1,49 @@
-import { useEffect } from "react";
-import { Marker } from "react-leaflet";
+import { useEffect } from 'react';
+import { Marker } from 'react-leaflet';
 
-import PopUpWithLocation from "../Components/PopUpWithLocation/PopUpWithLocation";
-import { updateAccepted } from "../Database";
+import PopUpWithLocation from '../Components/PopUpWithLocation/PopUpWithLocation';
+import { updateAccepted } from '../Database';
 
-export default function AddMarker({ ev, position , user}) {
-  /*
+export default function AddMarker({ event, position, user }) {
+	/*
 	description: string
 	position : {
 	 	lat: -6.17651,
 	 	lng: 106.790108,
 	 }
 	*/
-  useEffect(() => {
-    console.log("user 1", user);
-  }, []);
-  const acceptEvent = (e) => {
-    e.preventDefault();
-    const currentUid = sessionStorage.getItem("uid");
-    if (currentUid !== ev.uid) {
-      if (ev.accepted_uids && ev.accepted_uids.length > 0) {
-        if (!ev.accepted_uids.includes(currentUid)) {
-          ev.accepted_uids.push(currentUid);
-          updateAccepted(ev.uid, currentUid);
-        } else {
-          alert("You have accepted this event");
-        }
-      } else {
-        const uids = ev.accepted_uids
-          ? (ev.accepted_uids[0] = currentUid)
-          : [currentUid];
-        updateAccepted(ev.uid, uids);
-      }
-    } else {
-      alert("Cannot accept your own event");
-    }
-  };
-  return position === null ? null : (
-    <Marker position={position}>
-      <PopUpWithLocation
-        user={user}
-        ev={ev}
-        position={position}
-        acceptCallback={acceptEvent}
-      />
-    </Marker>
-  );
+	useEffect(() => {
+		console.log('user 1', user);
+	}, []);
+	const acceptEvent = (e) => {
+		e.preventDefault();
+		const currentUid = sessionStorage.getItem('uid');
+		if (currentUid !== event.uid) {
+			if (event.accepted_uids && event.accepted_uids.length > 0) {
+				if (!event.accepted_uids.includes(currentUid)) {
+					event.accepted_uids.push(currentUid);
+					updateAccepted(event.uid, currentUid);
+				} else {
+					alert('You have accepted this event');
+				}
+			} else {
+				const uids = event.accepted_uids
+					? (event.accepted_uids[0] = currentUid)
+					: [currentUid];
+				updateAccepted(event.uid, uids);
+			}
+		} else {
+			alert('Cannot accept your own event');
+		}
+	};
+	return position === null ? null : (
+		<Marker position={position}>
+			<PopUpWithLocation
+				user={user}
+				ev={event}
+				position={position}
+				acceptCallback={acceptEvent}
+			/>
+		</Marker>
+	);
 }
