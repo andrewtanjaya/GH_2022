f9d3bc0757c3dcadd88ba185d6c7f4ff2eae142d
DZ <adrianxwijaya@gmail.com>
2022-08-28 07:58:04 +0700

    event popup done

--- src/Constants.js
+++ src/Constants.js
@@ -11,18 +11,16 @@ export const CUSTOM = 'custom';
 
 export const DEFAULT_EVENT_TITLE = 'HELLO!';
 
-export const FAINT_ICON =
-  './asset/faint-icon.png';
-export const ROBBERY_ICON =
-  './asset/thief-icon.png';
-export const CAR_ACCIDENT_ICON =
-  './asset/accident-icon.png';
-export const FIRE_BREAKOUT_ICON =
-  './asset/fire-icon.png';
-export const CUSTOM_ICON =
-  './asset/help-icon.png';
+export const FAINT_ICON = './asset/faint-icon.png';
+export const ROBBERY_ICON = './asset/thief-icon.png';
+export const CAR_ACCIDENT_ICON = './asset/accident-icon.png';
+export const FIRE_BREAKOUT_ICON = './asset/fire-icon.png';
+export const CUSTOM_ICON = './asset/help-icon.png';
 
-export const FAINT_DESCRIPTION = "Someone with first aid knowlegde is required."
-export const ROBBERY_DESCRIPTION = "I lost my purse, did anyone see someone suspicious?"
-export const CAR_ACCIDENT_DESCRIPTION = "Need someone with first aid knowledge"
-export const FIRE_BREAKOUT_DESCRIPTION = "Help me distinguish the fire, and call fire fighter"
+export const FAINT_DESCRIPTION =
+  'Someone with first aid knowledge is required.';
+export const ROBBERY_DESCRIPTION =
+  'I lost my purse, did anyone see someone suspicious?';
+export const CAR_ACCIDENT_DESCRIPTION = 'Need someone with first aid knowledge';
+export const FIRE_BREAKOUT_DESCRIPTION =
+  'Help me distinguish the fire, and call fire fighter';
